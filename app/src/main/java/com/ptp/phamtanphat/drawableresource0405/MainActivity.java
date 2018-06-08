baafd000f7b6462505be2688e3e15797cfc28d58
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-08 18:20:38 +0700

    "Drawable Shape"

--- app/src/main/java/com/ptp/phamtanphat/drawableresource0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/drawableresource0405/MainActivity.java
@@ -1,13 +1,29 @@
 package com.ptp.phamtanphat.drawableresource0405;
 
+import android.graphics.drawable.ClipDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgClipDrawable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgClipDrawable = findViewById(R.id.imageviewclipdrawable);
+
+        final ClipDrawable clipDrawable = (ClipDrawable) imgClipDrawable.getDrawable();
+
+//        imgClipDrawable.setImageLevel();
+
+        imgClipDrawable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clipDrawable.setLevel(clipDrawable.getLevel() + 1000);
+            }
+        });
     }
 }
